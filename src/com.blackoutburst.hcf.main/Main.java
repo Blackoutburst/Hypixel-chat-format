a2812e6091145e4d8de1f3fee63e595f8af1a55b
Blackoutburst <blackoutburst@hotmail.com>
2022-02-10 14:28:16 +0100

    Full plugin lol

---
+++ src/com.blackoutburst.hcf.main/Main.java
@@ -0,0 +1,19 @@
+package com.blackoutburst.hcf.main;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+        event.setFormat("%s: %s");
+    }
+}
